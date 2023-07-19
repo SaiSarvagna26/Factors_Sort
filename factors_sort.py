1cc40aa13449b940e9c33de75a3a2a7a73c23098
GARRE SAI SARVAGNA SADVIK <88706307+SaiSarvagna26@users.noreply.github.com>
2023-07-19 18:42:12 +0530

    factors_sort.py

--- factors_sort.py
+++ factors_sort.py
@@ -1,16 +1,22 @@
-# A=list(map(int,input().split()))
-A=[6,8,9]
+import math
 
-dic = {}
-for i in A:
-    dic[i] = 0
-
-for i in A:
-    count=2
-    for j in range(2,i):
-        if i%j==0:
-            count+=1
-    dic[i] = count
-
-print(dic)
+def count_factors(n):
+    count=0
+    sqrt_n=int(math.sqrt(n))
     
+    for i in range(1,sqrt_n + 1):
+        if n %i==0:
+            count+=1
+            if i!=n//i:
+                count+=1
+    
+    return count
+
+def factors_sort(A):
+    factor_counts = [(count_factors(num), num) for num in A]
+    factor_counts.sort(key=lambda x: (x[0],x[1]))
+    return [val for count,val in factor_counts]
+
+A1 = list(map(int,input().split()))
+print(factors_sort(A1)) 
+
